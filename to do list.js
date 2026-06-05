d2874aa4822df4cc4d1b20377796a5016269e7aa
marmlu <dagnenuhamin987@gmail.com>
2026-06-05 17:47:28 +0300

    Add js code that add the input value to the container of the list

--- to do list.js
+++ to do list.js
@@ -1,3 +1,13 @@
 const taskInput = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
+
+button.addEventListener("click", () => {
+  let valueStore = taskInput.value;
+  console.log(valueStore);
+  const li = document.createElement("li");
+  li.className =
+    "bg-gray-500 text-white p-5 rounded-xl shadow-md hover:bg-gray-400 transition duration-300 cursor-pointer";
+  li.textContent = valueStore;
+  taskList.appendChild(li);
+});
