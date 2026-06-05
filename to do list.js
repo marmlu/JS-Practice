9ed01009f8da0fa094c1d1fed608a95b741fc458
marmlu <dagnenuhamin987@gmail.com>
2026-06-05 17:59:09 +0300

    Try to not add empty task on the container

--- to do list.js
+++ to do list.js
@@ -4,10 +4,14 @@ const taskList = document.getElementById("taskList");
 
 button.addEventListener("click", () => {
   let valueStore = taskInput.value;
-  console.log(valueStore);
   const li = document.createElement("li");
   li.className =
     "bg-gray-500 text-white p-5 rounded-xl shadow-md hover:bg-gray-400 transition duration-300 cursor-pointer";
   li.textContent = valueStore;
   taskList.appendChild(li);
+  taskInput.value = "";
+  if (taskInput.value === "") {
+    let currentLength = li[li.length - 1];
+    taskList.remove(li[currentLength]);
+  }
 });
