df71cd605cd226d5ec821653f0c18d68ea56f7e4
marmlu <dagnenuhamin987@gmail.com>
2026-06-06 14:27:17 +0300

    Make the task to appear as uppercase

--- to do list.js
+++ to do list.js
@@ -3,14 +3,16 @@ const button = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
 button.addEventListener("click", () => {
-  let valueStore = taskInput.value;
+  let valueStore = taskInput.value.toUpperCase();
   if (taskInput.value.trim() === "") {
     return;
   }
-  const li = document.createElement("li");
+  let li = document.createElement("li");
+
   li.className =
     "bg-gray-500 text-white p-5 rounded-xl shadow-md hover:bg-gray-400 transition duration-300 cursor-pointer";
   li.textContent = valueStore;
   taskList.appendChild(li);
+  console.log(li);
   taskInput.value = "";
 });
