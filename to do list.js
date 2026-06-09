96b329e21e735c703798f6e2b2f70904661b638d
marmlu <dagnenuhamin987@gmail.com>
2026-06-09 19:22:04 +0300

    Add delete button inside js li to remove the task

--- to do list.js
+++ to do list.js
@@ -1,7 +1,7 @@
 const taskInput = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
-function taskAdd() {
+function addTask() {
   let valueStore = taskInput.value.toUpperCase();
   if (taskInput.value.trim() === "") {
     return;
@@ -9,16 +9,25 @@ function taskAdd() {
   let li = document.createElement("li");
 
   li.className =
-    "bg-gray-500 text-white p-5 rounded-xl shadow-md hover:bg-gray-400 transition duration-300 cursor-pointer";
+    "flex justify-between items-center bg-gray-500 text-white p-5 rounded-xl shadow-md hover:bg-gray-400 transition duration-300 cursor-pointer";
+
   li.textContent = valueStore;
+  let delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.className =
+    "bg-red-500 hover:bg-red-400 text-white font-semibold px-4 py-2 rounded-xl transition duration-300";
+  delBtn.addEventListener("click", function () {
+    li.remove();
+  });
+  li.appendChild(delBtn);
   taskList.appendChild(li);
-  console.log(li);
+  console.log("Added:", valueStore);
   taskInput.value = "";
 }
-button.addEventListener("click", taskAdd);
-document.addEventListener("keydown", function (event) {
+button.addEventListener("click", addTask);
+taskInput.addEventListener("keydown", function (event) {
   let key = event.key;
   if (key === "Enter") {
-    taskAdd();
+    addTask();
   }
 });
