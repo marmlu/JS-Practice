71e9c29abf3b9ae967e06a2f6385e43c51d4a0bb
marmlu <dagnenuhamin987@gmail.com>
2026-06-11 16:35:15 +0300

    After refresh make previous tasks appear on the page

--- to do list.js
+++ to do list.js
@@ -7,35 +7,21 @@ function addTask() {
   if (taskInput.value.trim() === "") {
     return;
   }
-  let li = document.createElement("li");
-
-  li.className =
-    "flex justify-between items-center bg-gray-500 text-white p-5 rounded-xl shadow-md hover:bg-gray-400 transition duration-300 cursor-pointer";
-
-  li.textContent = taskText;
-  let delBtn = document.createElement("button");
-  delBtn.textContent = "❌";
-  delBtn.className =
-    "bg-red-500 hover:bg-red-400 text-white font-semibold px-4 py-2 rounded-xl transition duration-300";
-  delBtn.addEventListener("click", function (event) {
-    event.stopPropagation();
-    let shouldDelete = confirm("Delete this task?");
-    if (shouldDelete) {
-      li.remove();
-      console.log("Task Deleted");
-    }
-  });
-  li.appendChild(delBtn);
-  taskList.appendChild(li);
   tasks.push(taskText);
-  localStorage.setItem("tasks", JSON.strigify(tasks));
-  li.addEventListener("click", function () {
-    li.classList.toggle("line-through");
-    li.classList.toggle("opacity-50");
-    console.log("Task completed");
-  });
+  createTask(taskText);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   taskInput.value = "";
   console.log("Added:", taskText);
+}
+function loadTask() {
+  let savedTasks = JSON.parse(localStorage.getItem("tasks"));
+  if (!savedTasks) {
+    return;
+  }
+  savedTasks.forEach(function (task) {
+    tasks.push(task);
+    createTask(task);
+  });
 }
 button.addEventListener("click", addTask);
 taskInput.addEventListener("keydown", function (event) {
@@ -44,3 +30,41 @@ taskInput.addEventListener("keydown", function (event) {
     addTask();
   }
 });
+function createTask(taskText) {
+  let li = document.createElement("li");
+
+  li.className =
+    "flex justify-between items-center bg-gray-500 text-white p-5 rounded-xl shadow-md hover:bg-gray-400 transition duration-300 cursor-pointer";
+
+  li.textContent = taskText;
+
+  let delBtn = document.createElement("button");
+
+  delBtn.textContent = "❌";
+
+  delBtn.className =
+    "bg-red-500 hover:bg-red-400 text-white font-semibold px-4 py-2 rounded-xl transition duration-300";
+
+  delBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
+
+    let shouldDelete = confirm("Delete this task?");
+
+    if (shouldDelete) {
+      li.remove();
+      console.log("Task Deleted");
+    }
+  });
+
+  li.appendChild(delBtn);
+
+  taskList.appendChild(li);
+
+  li.addEventListener("click", function () {
+    li.classList.toggle("line-through");
+    li.classList.toggle("opacity-50");
+
+    console.log("Task completed");
+  });
+}
+loadTask();
