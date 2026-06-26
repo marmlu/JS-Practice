f7c6d1e19444a18657d6fd467e69a95d0f70d3b2
marmlu <dagnenuhamin987@gmail.com>
2026-06-26 17:18:30 +0300

    Do the html of color picker app and select its id in it's js

---
+++ Color Picker.js
@@ -0,0 +1,13 @@
+const colorInput = document.getElementById("colorInput");
+const colorPreview = document.getElementById("colorPreview");
+
+const hexValue = document.getElementById("hexValue");
+const rgbValue = document.getElementById("rgbValue");
+const hslValue = document.getElementById("hslValue");
+
+const copyBtn = document.getElementById("copyBtn");
+const randomBtn = document.getElementById("randomBtn");
+const saveBtn = document.getElementById("saveBtn");
+const themeBtn = document.getElementById("themeBtn");
+
+const favorites = document.getElementById("favorites");
