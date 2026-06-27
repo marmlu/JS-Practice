2797eb2eacfe68096686aac34afb54ead43baf84
marmlu <dagnenuhamin987@gmail.com>
2026-06-27 20:06:35 +0300

    Add codes that changes the preview box color when a user selects a color, displays HEX, RGB, and HSL valuses and shows the default color when the page first loads

--- Color Picker.js
+++ Color Picker.js
@@ -11,3 +11,65 @@ const saveBtn = document.getElementById("saveBtn");
 const themeBtn = document.getElementById("themeBtn");
 
 const favorites = document.getElementById("favorites");
+
+function updateColor() {
+  let color = colorInput.value;
+  colorPreview.style.backgroundColor = color;
+  hexValue.textContent = `HEX: ${color}`;
+  rgbValue.textContent = `RGB: ${hexToRGB(color)}`;
+  hslValue.textContent = `HSL: ${hexToHSL(color)}`;
+}
+function hexToRGB(hex) {
+  let color = colorInput.value;
+  color = color.replace("#", "");
+  let r = parseInt(color.substring(0, 2), 16);
+  let g = parseInt(color.substring(2, 4), 16);
+  let b = parseInt(color.substring(4, 6), 16);
+  return `(${r}, ${g}, ${b})`;
+}
+function hexToHSL(hex) {
+  let color = colorInput.value;
+  color = color.replace("#", "");
+  let r = parseInt(color.substring(0, 2), 16);
+  let g = parseInt(color.substring(2, 4), 16);
+  let b = parseInt(color.substring(4, 6), 16);
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let max = Math.max(r, g, b);
+  let min = Math.min(r, g, b);
+
+  let h,
+    s,
+    l = (max + min) / 2;
+  let d = max - min;
+
+  if (d === 0) {
+    h = s = 0;
+  } else {
+    s = d / (1 - Math.abs(2 * l - 1));
+
+    switch (max) {
+      case r:
+        h = ((g - b) / d) % 6;
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+
+    h = Math.round(h * 60);
+    if (h < 0) h += 360;
+  }
+
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+
+  return `(${h}, ${s}%, ${l}%)`;
+}
+
+colorInput.addEventListener("change", updateColor);
+updateColor();
